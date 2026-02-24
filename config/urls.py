9b01d16e08cb644200c7069eabdd948f59d7a844
tsion-zewdu <tsionzewdu461@gmail.com>
2026-02-24 02:13:52 -0900

    Update Database

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('events.urls')),
+    path('api-auth/', include('rest_framework.urls')),  
 ]
 
