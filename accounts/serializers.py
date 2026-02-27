012ed771d96d46c75a9a927198f6d8a843b352a1
tsion-zewdu <tsionzewdu461@gmail.com>
2026-02-26 23:49:37 -0900

    Add user registration API endpoint

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,7 +2,17 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
