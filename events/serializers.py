2890c0393572c7bcc8b5c8de0c72a4263bb1581b
tsion-zewdu <tsionzewdu461@gmail.com>
2026-02-27 14:24:15 -0900

    Added RSVP count to event serializer

--- events/serializers.py
+++ events/serializers.py
@@ -2,14 +2,24 @@ from rest_framework import serializers
 from .models import Event, RSVP
 
 
-class EventSerializer(serializers.ModelSerializer):
+"""class EventSerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source='created_by.username')
 
     class Meta:
         model = Event
         fields = ['id', 'title', 'description', 'date', 'location', 'created_by']
-        read_only_fields = ['creator']
+        read_only_fields = ['creator']"""
 
+class EventSerializer(serializers.ModelSerializer):
+    rsvp_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+    def get_rsvp_count(self, obj):
+        return obj.rsvps.count()
+    
 class RSVPSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
 
