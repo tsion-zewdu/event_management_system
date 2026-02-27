9b770fb8dbaa1bce3d4bc08c17b1fd410684c41a
tsion-zewdu <tsionzewdu461@gmail.com>
2026-02-27 14:13:14 -0900

    Added RSVP API endpoints

--- events/urls.py
+++ events/urls.py
@@ -3,12 +3,21 @@ from .views import (
     EventListCreateView,
     EventDetailView,
     RSVPCreateView,
-    EventRSVPListView
+    EventRSVPListView,
+    RSVPUpdateDeleteView
 )
 
-urlpatterns = [
+"""urlpatterns = [
     path('events/', EventListCreateView.as_view(), name='event-list'),
     path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
     path('events/<int:event_id>/rsvps/', EventRSVPListView.as_view(), name='event-rsvps'),
     path('rsvp/', RSVPCreateView.as_view(), name='rsvp-create'),
+]"""
+
+urlpatterns = [
+    path('', EventListCreateView.as_view(), name='event-list'),
+    path('<int:pk>/', EventDetailView.as_view(), name='event-detail'),
+    path('events/<int:event_id>/rsvps/', EventRSVPListView.as_view(), name='event-rsvps'),
+    path('rsvp/', RSVPCreateView.as_view(), name='rsvp-create'),
+    path('rsvp/<int:pk>/', RSVPUpdateDeleteView.as_view(), name='rsvp-update-delete'),
 ]
